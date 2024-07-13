e0afdf509b111af369a47d247b3213b1b9464b08
scribam <scribam@users.noreply.github.com>
2024-07-13 18:03:39 +0200

    Allow static linking with MSVC

--- C/src/libatrac9.h
+++ C/src/libatrac9.h
@@ -4,13 +4,19 @@ extern "C" {
 #endif
 
 #ifdef _MSC_VER
-#ifdef COMPILING_DLL 
-#define DLLEXPORT __declspec(dllexport)
+#define LIBATRAC9_DLLEXPORT __declspec(dllexport)
+#define LIBATRAC9_DLLIMPORT __declspec(dllimport)
 #else
-#define DLLEXPORT __declspec(dllimport)  
+#define LIBATRAC9_DLLEXPORT
+#define LIBATRAC9_DLLIMPORT
 #endif
+
+#if defined(LIBATRAC9_STATIC_DEFINE)
+#define LIBATRAC9_EXPORT
+#elif defined(COMPILING_DLL)
+#define LIBATRAC9_EXPORT LIBATRAC9_DLLEXPORT
 #else
-#define DLLEXPORT
+#define LIBATRAC9_EXPORT LIBATRAC9_DLLIMPORT
 #endif
 
 #define ATRAC9_CONFIG_DATA_SIZE 4
@@ -26,13 +32,13 @@ typedef struct {
 	unsigned char configData[ATRAC9_CONFIG_DATA_SIZE];
 } Atrac9CodecInfo;
 
-DLLEXPORT void* Atrac9GetHandle(void);
-DLLEXPORT void Atrac9ReleaseHandle(void* handle);
+LIBATRAC9_EXPORT void* Atrac9GetHandle(void);
+LIBATRAC9_EXPORT void Atrac9ReleaseHandle(void* handle);
 
-DLLEXPORT int Atrac9InitDecoder(void* handle, unsigned char *pConfigData);
-DLLEXPORT int Atrac9Decode(void* handle, const unsigned char *pAtrac9Buffer, short *pPcmBuffer, int *pNBytesUsed);
+LIBATRAC9_EXPORT int Atrac9InitDecoder(void* handle, unsigned char *pConfigData);
+LIBATRAC9_EXPORT int Atrac9Decode(void* handle, const unsigned char *pAtrac9Buffer, short *pPcmBuffer, int *pNBytesUsed);
 
-DLLEXPORT int Atrac9GetCodecInfo(void* handle, Atrac9CodecInfo *pCodecInfo);
+LIBATRAC9_EXPORT int Atrac9GetCodecInfo(void* handle, Atrac9CodecInfo *pCodecInfo);
 
 #ifdef __cplusplus
 }
